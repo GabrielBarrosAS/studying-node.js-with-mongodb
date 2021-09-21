13ff45be54fdb2164831eb2658faba10af2ec3bf
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-09-21 15:38:48 -0300

    Adaptação para tentar usar heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const userRoutes = require('./routes/userRoutes.js')
 const authenticatedUserRoutes = require('./routes/authenticatedUserRoutes')
 
-const PORT = 3000;
+const PORT = pross.env.PORT || 3000;
 const HOST = '0.0.0.0'
 
 const app = express()
