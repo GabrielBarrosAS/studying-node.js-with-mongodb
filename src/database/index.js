3586b50af379d43996eed869a5a29b3ededed8a5
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-09-21 16:20:13 -0300

    Adaptação para tentar usar heroku 4

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://root:<root>@cluster0.mu7dp.mongodb.net/nodemongo?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://root:root@cluster0.mu7dp.mongodb.net/nodemongo?retryWrites=true&w=majority",
                 {useNewUrlParser: true })
                 .then(() => console.log('MongoDB Connected'))
                 .catch(err => console.log(err));
+
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
