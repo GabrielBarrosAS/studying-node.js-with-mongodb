777898095c4fb2f83ea374c306ab9c6b928092cc
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-09-21 11:37:41 -0300

    Servidor consumindo as rotas de autenticação

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userRoutes = require('./routes/userRoutes.js')
+const authenticatedUserRoutes = require('./routes/authenticatedUserRoutes')
 
 const PORT = 3000;
 const HOST = '0.0.0.0'
@@ -10,5 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/',userRoutes)
+app.use('/authenticated',authenticatedUserRoutes)
+
 
 app.listen(PORT,HOST)
