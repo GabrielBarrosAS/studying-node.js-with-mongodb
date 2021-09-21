5309f99247c3162fbf22da1391f8ee86f42269ab
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-09-21 16:16:28 -0300

    Adaptação para tentar usar heroku 3

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 
-
-const DB_URI = process.env.MONGODB_URI
-mongoose.connect(DB_URI,
+mongoose.connect("mongodb+srv://root:<root>@cluster0.mu7dp.mongodb.net/nodemongo?retryWrites=true&w=majority",
                 {useNewUrlParser: true })
                 .then(() => console.log('MongoDB Connected'))
                 .catch(err => console.log(err));
